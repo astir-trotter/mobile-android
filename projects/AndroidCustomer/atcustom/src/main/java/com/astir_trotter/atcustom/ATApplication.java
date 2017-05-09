7f3960add7304b9d8ae555d391204fca80b0e60f
Yonis Larsson <yonis.larsson.biz@gmail.com>
2017-05-09 21:59:24 +0800

    - min sdk version -> 19 - navigation view update - translucent status bar update

--- projects/AndroidCustomer/atcustom/src/main/java/com/astir_trotter/atcustom/ATApplication.java
+++ projects/AndroidCustomer/atcustom/src/main/java/com/astir_trotter/atcustom/ATApplication.java
@@ -7,8 +7,6 @@ import com.astir_trotter.atcustom.component.AppInfo;
 import com.astir_trotter.atcustom.singleton.Cache;
 import com.astir_trotter.atcustom.singleton.lang.MultiLangStringRes;
 import com.astir_trotter.atcustom.singleton.lang.base.Language;
-import com.astir_trotter.atcustom.singleton.theme.MultiThemeColorRes;
-import com.astir_trotter.atcustom.singleton.theme.base.Theme;
 import com.astir_trotter.atcustom.ui.activity.base.AutoCrashReporter;
 
 /**
@@ -46,7 +44,7 @@ public abstract class ATApplication extends Application {
     }
 
     protected boolean isDebug() {
-        return true;
+        return BuildConfig.DEBUG;
     }
 
     @NonNull
